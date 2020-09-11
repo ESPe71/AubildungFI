653889778f0958a1cda2b5054d27dc37b56ec002
Enrico S. Penetti <enrico@penetti.de>
2020-09-11 08:47:17 +0200

    Refaktoring TowersOfHanoiTest

--- src/test/java/de/penetti/ausbildung/fachinformatiker/uebungen/TowersOfHanoiTest.java
+++ src/test/java/de/penetti/ausbildung/fachinformatiker/uebungen/TowersOfHanoiTest.java
@@ -1,10 +1,7 @@
 package de.penetti.ausbildung.fachinformatiker.uebungen;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class TowersOfHanoiTest {
   @ParameterizedTest
