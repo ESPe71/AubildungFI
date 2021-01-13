7db9b7af3a470f6780225e4e4d5fadc830beca87
Enrico S. Penetti <enrico@penetti.de>
2021-01-13 13:23:08 +0100

    Refaktoring

--- Graphic/src/main/java/de/penetti/fi/ae/graphic/model/Drawing.java
+++ Graphic/src/main/java/de/penetti/fi/ae/graphic/model/Drawing.java
@@ -7,7 +7,7 @@ import java.util.Collection;
 import java.util.List;
 
 public class Drawing implements Observable {
-  private List<Primitive> primitives = new ArrayList<>();
+  private final List<Primitive> primitives = new ArrayList<>();
   private Collection<Observer> observers;
 
   public int size() {
